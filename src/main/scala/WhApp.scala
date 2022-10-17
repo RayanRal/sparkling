cbf39b52650db0f5d4db4430cfbaef5671b2c464
leonidchashnikov <rayanral@gmail.com>
2022-10-17 23:16:01 +0100

    Create Spark app

---
+++ src/main/scala/WhApp.scala
@@ -0,0 +1,24 @@
+package com.rayanral
+
+import org.apache.spark.sql._
+
+object WhApp {
+
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder.master("spark://127.0.0.1:7077")
+      .config("spark.cores.max", "2")
+      .config("spark.executor.memory", "2g")
+      .appName("Warhammer 40k").getOrCreate()
+    import spark.implicits._
+
+    val testData = Seq(
+      WarhammerUnit("Orks", "Trukk", "Red", 12),
+      WarhammerUnit("Orks", "Trukk", "Blue", 12),
+      WarhammerUnit("Blood Angels", "Rhino", "Red", 12),
+      WarhammerUnit("Adeptus Astartes", "Librarian", "Ultramarine", 6),
+    )
+    val testDf = spark.createDataset(testData)
+    new Accelerator(spark).redGoezFasta(testDf).show()
+  }
+
+}
